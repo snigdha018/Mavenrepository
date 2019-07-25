f1479529981ba998aa96f0a722ab52c6aa14f2f3
snigdha018 <snigdha.mishra018@gmail.com>
2019-07-25 12:29:05 +0530

    done

---
+++ Mavenproject/src/test/java/Snigdha.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+
+public class Snigdha 
+{
+	
+	
+		@Test
+	  public void customerTest()
+	  {
+			System.out.println("create customer test");
+	  }
+		@Test
+		public void modifycustometest()
+		{
+			System.out.println("modify customettest");
+		}
+		@Test
+		public void deletecustometTest()
+		{
+			System.out.println("delete customer test");
+		}
+	}
+
